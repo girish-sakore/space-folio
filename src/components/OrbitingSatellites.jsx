a35396589928b29ca9e65d0815583afa12908d8a
g-WSL <gsakore22@gmail.com>
2025-07-08 14:09:39 +0530

    responsive satellites

--- src/components/OrbitingSatellites.jsx
+++ src/components/OrbitingSatellites.jsx
@@ -7,13 +7,13 @@ const OrbitingSatellites = () => {
         alt="Satellite"
         className="satellite satellite-1"
         src="https://static.vecteezy.com/system/resources/previews/033/529/060/non_2x/starlink-satellite-ai-generative-free-png.png"
-        style={{ animation: 'spin-slow 60s linear infinite' }}
+        // style={{ animation: 'spin-slow 60s linear infinite' }}
       />
       <img
         alt="Satellite"
         className="satellite satellite-2"
         src="https://static.vecteezy.com/system/resources/previews/040/335/018/large_2x/ai-generated-satellite-isolated-on-transparent-background-free-png.png"
-        style={{ animation: 'spin-slow 60s linear infinite' }}
+        // style={{ animation: 'spin-slow 60s linear infinite' }}
       />
     </div>
   );
